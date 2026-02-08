8512ec3b86576dcbb941e84e3fe4da8d79d8a862
uppermooninfinity <uppermooninfinity@gmail.com>
2026-02-08 13:27:12 +0530

    Update superban_request.py

--- Oneforall/plugins/tools/superban_request.py
+++ Oneforall/plugins/tools/superban_request.py
@@ -5,8 +5,9 @@ from pyrogram.types import CallbackQuery
 async def request_superban(_, query: CallbackQuery):
     await query.answer()
     await query.message.edit_text(
-        "✦ ʜᴇʏ 🥀\n\n"
-        "⊚ ᴛᴀɢ ᴀ ᴜsᴇʀ ᴏʀ ɴsғᴡ sᴘᴀᴍᴍᴇʀ ᴏʀ ʀᴜʟᴇ ʙʀᴇᴀᴋᴇʀ ɪɴ ʏᴏᴜʀ ɢʀᴏᴜᴘ\n"
-        "➻ ᴛʜᴇɴ sᴇɴᴅ `/superban`\n\n"
-        "❖ ʏᴏᴜʀ ʀᴇǫᴜᴇsᴛ ᴡɪʟʟ ʙᴇ ᴀᴘᴘʀᴏᴠᴇᴅ ʙʏ ᴜs ✅"
+        "<blockquote><i><u>✦ ʜᴇʏ 🥀\n\n<u><i></blockquote>"
+        "<blockquote><i><u>⊚ ᴛᴀɢ ᴀ ᴜsᴇʀ ᴏʀ ɴsғᴡ sᴘᴀᴍᴍᴇʀ ᴏʀ ʀᴜʟᴇ ʙʀᴇᴀᴋᴇʀ ɪɴ ʏᴏᴜʀ ɢʀᴏᴜᴘ\n</u></i></blockquote>"
+        "<blockquote><i><u>➻ ᴛʜᴇɴ sᴇɴᴅ `/superban`\n\n</u></i></blockquote>"
+        "<blockquote><i><u>❖ ʏᴏᴜʀ ʀᴇǫᴜᴇsᴛ ᴡɪʟʟ ʙᴇ ᴀᴘᴘʀᴏᴠᴇᴅ ʙʏ ᴜs ✅</u></i></blockquote>"
+        "<blockquote><i><u>❖ to view superban logs join @docker_git_bit</u></i></blockquote>"
     )
