a76897f43621a4c0dad317bb328c8ed022b15c61
uppermooninfinity <uppermooninfinity@gmail.com>
2026-02-08 13:08:56 +0530

    Update start.py

--- Oneforall/utils/inline/start.py
+++ Oneforall/utils/inline/start.py
@@ -34,5 +34,8 @@ def private_panel(_):
         [
             InlineKeyboardButton(text=_["S_B_7"], callback_data="gib_source"),
         ],
+         [
+            InlineKeyboardButton(text=_["☘️ᴏᴡɴᴇʀ"], url="https://t.me/cyber_github"),
+        ],
     ]
     return buttons
