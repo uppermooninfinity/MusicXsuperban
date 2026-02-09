fdcd29510ca78d8ee7e7df41a4b05d2e017baf25
uppermooninfinity <uppermooninfinity@gmail.com>
2026-02-09 16:46:18 +0530

    Update start.py

--- Oneforall/utils/inline/start.py
+++ Oneforall/utils/inline/start.py
@@ -27,8 +27,6 @@ def private_panel(_):
         [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
         [
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
-        ],
-        [
             InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
         ],
         [
